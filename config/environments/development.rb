3dffc2abe9e23389c217819c3c350bd58c399c2a
Takumi Ozato <1992tkm0819@gmail.com>
2018-09-26 07:46:31 +0000

    twitter login

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,14 +37,14 @@ Rails.application.configure do
   config.assets.raise_runtime_errors = true
 
   # mailer setting
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'https://hampuku-takumi7dot.c9users.io' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :user_name => "test@gmail.com",
-    :password => "test",
+    :user_name => "1992tkm0819@gmail.com",
+    :password => "gikaakuzkyzqllty",
     :authentication => :plain,
     :enable_starttls_auto => true
   }
