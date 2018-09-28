8e5e5faaf85670a1ff6a3496376ac9deef9f7573
Takumi Ozato <1992tkm0819@gmail.com>
2018-09-28 01:12:29 +0000

    puma config

--- config/puma.rb
+++ config/puma.rb
@@ -4,7 +4,7 @@ threads threads_count, threads_count
 preload_app!
 rackup DefaultRackup
 port ENV['PORT'] || 3000
-environment ENV['RACK_ENV'] || 'development'
+environment ENV['RACK_ENV'] || 'production'
 on_worker_boot do
  ActiveRecord::Base.establish_connection
 end
