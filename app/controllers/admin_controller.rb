59bac41b76cc1aad3e19dbc934ffefb6137f066c
Takumi Ozato <1992tkm0819@gmail.com>
2018-09-29 04:17:50 +0000

    test

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,7 @@
 class AdminController < ApplicationController
   before_action :sign_in_required, only: [:index]
   def index
-    @user = User.find(current_user.id)
+    @user = User.find(25)
     @books = @user.books
   end
 end
