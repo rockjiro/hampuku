2a6b9dfd3b2a2221cc3050c45d189ee73dd03714
Takumi Ozato <1992tkm0819@gmail.com>
2018-09-29 10:08:03 +0000

    book exists check

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,6 +2,8 @@ class AdminController < ApplicationController
   before_action :sign_in_required, only: [:index]
   def index
     @user = User.find(current_user.id)
-    @books = @user.books
+    if @user.books.any?
+      @books = @user.books
+    end
   end
 end
