08dc6cb8c79153bc1e385105dbc9e7a1d73fe9ca
Takumi Ozato <1992tkm0819@gmail.com>
2018-10-04 03:28:17 +0000

    assets precompile

---
+++ public/assets/frontend/book_create-629f923f8d015b38a14f6aafd014d1e533f48949b12adc1ebaf305b6f4f084cb.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        isActive: false
+    },
+    methods: {
+        doActive: function() {
+            this.isActive = !this.isActive
+        }
+    }
+})
+;
