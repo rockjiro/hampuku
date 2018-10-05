a945473d42c1f3ff2041b4c5f5455ef53b3b5138
Takumi Ozato <1992tkm0819@gmail.com>
2018-10-05 13:51:48 +0000

    book index style update

--- app/assets/javascripts/frontend/actions-btn.js
+++ app/assets/javascripts/frontend/actions-btn.js
@@ -1,13 +1,9 @@
 var app = new Vue({
     el: '#app',
     data: {
-        isActive: false,
         isOpen: false
     },
     methods: {
-        doActive: function() {
-            this.isActive = !this.isActive
-        },
         doOpen: function() {
             this.isOpen = !this.isOpen
         }
--- public/assets/frontend/actions-btn-a9f57b73bff85ac6557ea53432b69583842ba1733a6f2717e31917d47ee25742.js
+++ public/assets/frontend/actions-btn-a9f57b73bff85ac6557ea53432b69583842ba1733a6f2717e31917d47ee25742.js
@@ -1,15 +1,12 @@
 var app = new Vue({
     el: '#app',
     data: {
-        isActive: false,
         isOpen: false
     },
     methods: {
-        doActive: function() {
-            this.isActive = !this.isActive
-        },
         doOpen: function() {
             this.isOpen = !this.isOpen
         }
     }
 })
+;
